8caac0d4d29e298400048384557198f05d43909f
Vinay Singh <vinaysingh8866@gmail.com>
2022-08-31 13:15:55 -0400

    added tweet blueprint

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from models.InvalidToken import getInvalidToken
-from models.Tweet import addNewTweet, delAddedTweet, getAddedTweets, getTweet, getUserAddedTweets, updateAddedTweet
-from models.User import User, getUsers, addUser
-from flask import Flask, request, jsonify
-import re
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, \
-    jwt_refresh_token_required, create_refresh_token, get_raw_jwt
+from models.Tweet import getTweet
+from models.User import User
+from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from blueprints.blueprintAuth import blueprint_auth
+from blueprints.blueprintTweet import blueprint_tweet
+
 app = Flask(__name__)
+#DB Settings
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///socialMedia.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+#DB tables
 User = User(db)
 Tweet = getTweet(db)
 InvalidToken = getInvalidToken(db)
+
+#Setup og config for use in app
 app.config["user"] = User
 app.config["tweet"] = Tweet
 app.config["db"] = db
@@ -21,68 +26,19 @@ app.config["invalidToken"] = InvalidToken
 app.config["JWT_SECRET_KEY"] = "test123123Testabcrfggajkshhk"
 app.config["JWT_BLACKLIST_ENABLED"] = True
 app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
+#Register Token Managemeent
 jwt = JWTManager(app)
 
-
-
+#Register BluePrints
 app.register_blueprint(blueprint_auth)
+app.register_blueprint(blueprint_tweet)
 
+
+#BlackListing Token 
 @jwt.token_in_blacklist_loader
 def check_if_blacklisted_token(decrypted):
     jti = decrypted["jti"]
     return InvalidToken.is_invalid(jti)
-
-@app.route("/api/all_tweets")
-def getTweets():
-    return jsonify(getAddedTweets(Tweet, User))
-
-
-@app.route("/api/add_tweet", methods=["POST"])
-@jwt_required
-def addTweet():
-    try:
-        title = request.json["title"]
-        content = request.json["content"]
-        if not (title and content):
-            return jsonify({"error": "Invalid request"})
-        uid = get_jwt_identity()
-        addNewTweet(Tweet, db, User, title, content, uid)
-        return jsonify({"success": "true"})
-    except Exception as e:
-        print(e)
-        return jsonify({"error": "Invalid Request"})
-
-
-@app.route("/api/delete_tweet/<tid>", methods=["DELETE"])
-@jwt_required
-def deleteTweet(tid):
-    try:
-        delAddedTweet(Tweet, db, tid)
-        return jsonify({"success": "true"})
-    except:
-        return jsonify({"error": "Invalid form"})
-
-@app.route("/api/user_tweets", methods=["POST"])
-@jwt_required
-def userTweets():
-    try:
-        uid = request.json["uid"]
-        tweets = getUserAddedTweets(Tweet, uid)
-        return jsonify(tweets)
-    except:
-        return jsonify({"error": "Invalid form"})
-
-
-@app.route("/api/update_tweet/<tid>", methods=["POST"])
-@jwt_required
-def updateTweet(tid):
-    content = request.json["content"]
-    try:
-        updateAddedTweet(Tweet, db, tid, content)
-        return jsonify({"success": "true"})
-    except:
-        return jsonify({"error": "Invalid form"})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ blueprints/blueprintTweet.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, request, jsonify,current_app
+from models.Tweet import addNewTweet, delAddedTweet, getAddedTweets, getUserAddedTweets, updateAddedTweet
+
+from models.User import addUser, getUsers
+import re
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, \
+    jwt_refresh_token_required, create_refresh_token, get_raw_jwt
+blueprint_tweet= Blueprint('blueprint_tweet', __name__)
+app = current_app
+
+
+@blueprint_tweet.route("/api/all_tweets")
+def getTweets():
+    with app.app_context():
+        User = current_app.config['user']
+        Tweet = current_app.config['tweet']
+        return jsonify(getAddedTweets(Tweet, User))
+
+
+@blueprint_tweet.route("/api/add_tweet", methods=["POST"])
+@jwt_required
+def addTweet():
+    with app.app_context():
+        db = current_app.config['db']
+        User = current_app.config['user']
+        Tweet = current_app.config['tweet']
+        try:
+            title = request.json["title"]
+            content = request.json["content"]
+            if not (title and content):
+                return jsonify({"error": "Invalid request"})
+            uid = get_jwt_identity()
+            addNewTweet(Tweet, db, User, title, content, uid)
+            return jsonify({"success": "true"})
+        except Exception as e:
+            print(e)
+            return jsonify({"error": "Invalid Request"})
+
+
+@blueprint_tweet.route("/api/delete_tweet/<tid>", methods=["DELETE"])
+@jwt_required
+def deleteTweet(tid):
+    with app.app_context():
+        db = current_app.config['db']
+        Tweet = current_app.config['tweet']
+        try:
+            delAddedTweet(Tweet, db, tid)
+            return jsonify({"success": "true"})
+        except:
+            return jsonify({"error": "Invalid form"})
+
+@blueprint_tweet.route("/api/user_tweets", methods=["POST"])
+@jwt_required
+def userTweets():
+    with app.app_context():
+        Tweet = current_app.config['tweet']
+        try:
+            uid = request.json["uid"]
+            tweets = getUserAddedTweets(Tweet, uid)
+            return jsonify(tweets)
+        except:
+            return jsonify({"error": "Invalid form"})
+
+
+@blueprint_tweet.route("/api/update_tweet/<tid>", methods=["POST"])
+@jwt_required
+def updateTweet(tid):
+    with app.app_context():
+        db = current_app.config['db']
+        Tweet = current_app.config['tweet']
+        content = request.json["content"]
+        try:
+            updateAddedTweet(Tweet, db, tid, content)
+            return jsonify({"success": "true"})
+        except:
+            return jsonify({"error": "Invalid form"})
