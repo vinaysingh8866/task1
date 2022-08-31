9c728ab815ed33e955361b4db2da3318c5e6ffcd
Vinay Singh <vinaysingh8866@gmail.com>
2022-08-31 11:39:34 -0400

    added login session management and its tests

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from models.User import User, getUsers, addUser
 from flask import Flask, request, jsonify
 import re
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, \
+    jwt_refresh_token_required, create_refresh_token, get_raw_jwt
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///socialMedia.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 User = User(db)
-#print(getUsers(User))
+# print(getUsers(User))
+app.config["JWT_SECRET_KEY"] = "test123123Testabcrfggajkshhk"
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
+jwt = JWTManager(app)
+
 
 @app.route("/api/register", methods=["POST"])
 def register():
@@ -23,13 +30,37 @@ def register():
         usersByEmail = list(filter(lambda x: (x["email"] == email), users))
         if len(usersByEmail) == 1:
             return jsonify({"error": "Invalid Request"})
-        
+
         if not re.match(r"[\w._]{5,}@\w{3,}\.\w{2,4}", email):
             return jsonify({"error": "Invalid email"})
-        addUser(db,User, uname, email, upassword)
+        addUser(db, User, uname, email, upassword)
         return jsonify({"success": True})
     except Exception as e:
         print(e)
+        return jsonify({"error": "Invalid Request"})
+
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    try:
+
+        upassword = request.json["upassword"]
+        email = request.json["email"]
+        if email and upassword:
+            user = list(filter(
+                lambda x: x["email"] == email and upassword == x["upassword"], getUsers(User)))
+            if len(user) == 1:
+
+                refresh_token = create_refresh_token(identity=user[0]["id"])
+                token = create_access_token(identity=user[0]["id"])
+                returnJson = jsonify(
+                    {"token": token, "refreshToken": refresh_token})
+                return returnJson
+            else:
+                return jsonify({"error": "Invalid Password"})
+        else:
+            return jsonify({"error": "Invalid Request"})
+    except Exception as e:
         return jsonify({"error": "Invalid Request"})
 
 
